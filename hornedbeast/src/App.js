d03f608b121f9f43a7ac769059bf2fde04d6ecab
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-06-08 16:36:27 +0300

    solve the lab3

--- hornedbeast/src/App.js
+++ hornedbeast/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './component/Header';
 import Main from './component/Main';
 import Footer from './component/Footer';
+//import 'bootstraps/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component {
--- hornedbeast/src/component/Header.js
+++ hornedbeast/src/component/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-          //  <div className='Header' >
-                <h1> Firist Animal </h1>
+            //  <div className='Header' >
+            <h1> Firist Animal </h1>
             //</div>
         );
 
